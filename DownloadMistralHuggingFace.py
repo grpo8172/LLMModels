dc3f2a13775712b5eef9748eee13186ade6c816c
grape17 <50038679+grpo8172@users.noreply.github.com>
2025-07-10 20:01:33 +1000

    Add files via upload

---
+++ DownloadMistralHuggingFace.py
@@ -0,0 +1,6 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "mistralai/Mistral-7B-v0.1"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
---
+++ DownloadPhi2HuggingFace.py
@@ -0,0 +1,6 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "microsoft/phi-2"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
